6fda24abfdc4a9ae3a2ee499bde92b5cfaea5835
bugpiggg <bugpigg@naver.com>
2024-07-01 23:56:37 +0900

    boj 30802

---
+++ boj/30802.java
@@ -0,0 +1,29 @@
+package algorithm.boj;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] tSizes = new int[6];
+        for (int i = 0; i < tSizes.length; i++) {
+            tSizes[i] = sc.nextInt();
+        }
+        int t = sc.nextInt();
+        int p = sc.nextInt();
+
+        int tCount = 0;
+        for (int i = 0; i < tSizes.length; i++) {
+            tCount += tSizes[i] / t;
+            if (tSizes[i] % t != 0) {
+                tCount += 1;
+            }
+        }
+
+        System.out.println(tCount);
+        System.out.println(n / p + " " + n % p);
+        sc.close();
+    }
+}

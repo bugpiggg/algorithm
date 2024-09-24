83bfe52cd2c216afce1a0af539d7cfffc34e293b
bugpiggg <bugpigg@naver.com>
2024-09-24 23:48:56 +0900

    boj 10171

---
+++ boj/_10171.java
@@ -0,0 +1,17 @@
+public class _10171 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\");
+        sb.append("\n");
+
+        sb.append(" )  ( ')");
+        sb.append("\n");
+
+        sb.append("(  /  )");
+        sb.append("\n");
+
+        sb.append(" \\(__)|");
+
+        System.out.println(sb);
+    }
+}

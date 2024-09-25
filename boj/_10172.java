2bbf48146ede43c087246e82e69c0afc89e17dd9
bugpiggg <bugpigg@naver.com>
2024-09-25 22:27:21 +0900

    boj 10172

---
+++ boj/_10172.java
@@ -0,0 +1,21 @@
+public class _10172 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|\\_/|");
+        sb.append("\n");
+
+        sb.append("|q p|   /}");
+        sb.append("\n");
+
+        sb.append("( 0 )\"\"\"\\");
+        sb.append("\n");
+
+        sb.append("|\"^\"`    |");
+        sb.append("\n");
+
+        sb.append("||_/=\\\\__|");
+
+        System.out.println(sb);
+    }
+}

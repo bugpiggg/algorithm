43ed1f228df22e44cced90fa56b0a70d8dc0cf09
bugpiggg <bugpigg@naver.com>
2024-10-07 23:54:28 +0900

    boj 27866

---
+++ boj/_27866.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class _27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] S = sc.nextLine().split("");
+        int i = sc.nextInt();
+
+        System.out.println(S[i-1]);
+
+        sc.close();
+    }
+}
